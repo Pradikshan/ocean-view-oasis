af79e7c2adc3dfa160de2557f5184d6ebd901821
Pradikshan <pradikshan234@gmail.com>
2024-01-29 19:04:39 +0530

    Added hero image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,25 @@
 const HomePage = () => {
     return (
 
+        <>
+        <div className="relative">
+            <div className="flex flex-col justify-center items-center">
+                <img src="../images/main_img2.jpg" alt="main" className="w-full h-auto opacity-80 z-0" />
+                <div className="z-10 absolute">
+                    <div className="flex flex-col justify-center items-center">
+                        <p className="font-bold text-lg md:text-4xl text-white ps-3">OCEAN VIEW OASIS</p>
+                        <p className="font-lg text-sm md:text-2xl text-white">Your Spacious Haven by the Shore</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            
+        </div>
+        </>
+      
+
     );
 }
 
