cf5cf05bb140753d5f521eb0711798096d5e7ebe
Pradikshan <pradikshan234@gmail.com>
2024-01-29 19:18:02 +0530

    Removed unused import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppRoutes } from './routes/routes';
-import FAQ from './pages/FAQ';
 
 function App() {
   return (
