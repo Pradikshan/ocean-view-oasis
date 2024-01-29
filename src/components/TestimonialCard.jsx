c436b8f3fd1f926418ea81d84deecd2069a66824
Pradikshan <pradikshan234@gmail.com>
2024-01-29 23:42:14 +0530

    Increased box shadow for the component

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,6 +1,6 @@
 const TestimonialCard = ({ image, name, testimonial}) => {
     return (
-        <div className="rounded-3xl shadow-lg m-8">
+        <div className="rounded-3xl shadow-xl m-8">
             <div className="flex flex-col justify-center items-center px-8 py-3">
                 <img src={image} alt="resident" className="rounded-full object-cover w-24 h-24" />
                 <p className="text-xl font-bold">{name}</p>
