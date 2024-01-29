22265a4b246e09f1b2f8502b975cc687ab047966
Pradikshan <pradikshan234@gmail.com>
2024-01-29 23:07:37 +0530

    Changed the layout from flex to grid

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -1,7 +1,27 @@
+import TestimonialCard from "./TestimonialCard";
 
 
 const TestimonialSection = () => {
     return (
-        
+        <div className="mt-28">
+            <div className="flex flex-col justify-center items-center">
+                <p className="text-4xl font-bold">Testimonials</p>
+                <p>Testimonials from our residents</p>
+            </div>
+            <div className="grid grid-cols-3 place-items-stretch">
+                <TestimonialCard />
+                <TestimonialCard />
+                <TestimonialCard />
+                <TestimonialCard />
+                <TestimonialCard />
+                <TestimonialCard />
+                <TestimonialCard />
+                <TestimonialCard />
+                
+                
+            </div>
+        </div>
     );
 }
+
+export default TestimonialSection;
