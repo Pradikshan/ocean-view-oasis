3d3e7a836ef8d68f331f10155d4f37fe10ba5794
Pradikshan <pradikshan234@gmail.com>
2024-01-29 23:37:34 +0530

    Added three testimonials

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -9,14 +9,19 @@ const TestimonialSection = () => {
                 <p>Testimonials from our residents</p>
             </div>
             <div className="grid grid-cols-3 place-items-stretch">
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
-                <TestimonialCard />
+                <TestimonialCard 
+                image={"../images/gandalf.jpg"}
+                name={"Gandalf the Grey"} 
+                testimonial={"Ocean View Oasis is a marvel! The soothing ocean vistas, spacious interiors, and enchanting atmosphere make it a truly magical retreat. Whether you're a wizard or a mere mortal, this apartment is a haven by the sea."}/>
+                <TestimonialCard 
+                image={"../images/aragorn5.jpg"}
+                name={"Aragorn, Son of Arathorn"}
+                testimonial={"In my journeys across realms and kingdoms, I have seldom found a residence as splendid as Ocean View Oasis. The majestic views of the vast ocean, the regal interiors, and the serene atmosphere create an abode fit for a king. Whether you seek respite or luxury, this apartment is a testament to the finer things in life."}/>
+                <TestimonialCard 
+                image={"../images/legolas5.jpg"}
+                name={"Legolas Greenleaf"}
+                testimonial={"Ocean View Oasis, where rhythmic tides meet Elvish elegance. With graceful interiors, this dwelling bridges nature and modernity, resonating with woodland spirit. A haven to commune with the essence of the sea and ancient tales."}/>
+
                 
                 
             </div>
