0ba23e6e758336853cba79a482433900a4974f71
Pradikshan <pradikshan234@gmail.com>
2024-01-29 23:13:15 +0530

    Added logic to pass values into the component as props

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,10 +1,10 @@
-const TestimonialCard = () => {
+const TestimonialCard = ({ image, name, testimonial}) => {
     return (
         <div className="rounded-3xl shadow-lg m-8">
             <div className="flex flex-col justify-center items-center px-8 py-3">
-                <img src="../images/gandalf.jpg" alt="resident" className="rounded-full object-cover w-24 h-24" />
-                <p className="text-xl font-bold">Gandalf the Grey</p>
-                <p className="pt-4">Ocean View Oasis is a marvel! The soothing ocean vistas, spacious interiors, and enchanting atmosphere make it a truly magical retreat. Whether you're a wizard or a mere mortal, this apartment is a haven by the sea</p>
+                <img src={image} alt="resident" className="rounded-full object-cover w-24 h-24" />
+                <p className="text-xl font-bold">{name}</p>
+                <p className="pt-4">{testimonial}</p>
             </div>
         </div>
 
