28169e9a0bbe6b0e5b546d9e6f60fb33f577f302
Pradikshan <pradikshan234@gmail.com>
2024-01-29 23:07:21 +0530

    Implemented layout for the TestimonialCard component

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,5 +1,14 @@
-const TestimonialCard = ({ image, name, testimonial }) => {
+const TestimonialCard = () => {
     return (
-        
+        <div className="rounded-3xl shadow-lg m-8">
+            <div className="flex flex-col justify-center items-center px-8 py-3">
+                <img src="../images/gandalf.jpg" alt="resident" className="rounded-full object-cover w-24 h-24" />
+                <p className="text-xl font-bold">Gandalf the Grey</p>
+                <p className="pt-4">Ocean View Oasis is a marvel! The soothing ocean vistas, spacious interiors, and enchanting atmosphere make it a truly magical retreat. Whether you're a wizard or a mere mortal, this apartment is a haven by the sea</p>
+            </div>
+        </div>
+
     );
 }
+
+export default TestimonialCard;
