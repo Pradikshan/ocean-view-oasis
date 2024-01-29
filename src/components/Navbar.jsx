1bd35cab5912096c9a1e1497bd271077690c07a7
Pradikshan <pradikshan234@gmail.com>
2024-01-29 13:35:58 +0530

    Implemented Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,10 @@ import { useState } from "react";
 
 const Navbar = () => {
 
-    const [isToggleMEnu, setIsToggleMenu] = useState(false);
+    
 
     return (
-        <div className="hidden md:flex flex-row w-full">
-            <div>
-                <img src="../public/images/logo.png" alt="logo" />
-            </div>
-
-            <div>
-                <ul>
-                    <li>Home</li>
-                </ul>
-            </div>
-
-        </div>
+        
     );
 
 }
