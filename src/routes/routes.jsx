7157e553cc19db5040e772a4595b30c438d5fe52
Pradikshan <pradikshan234@gmail.com>
2024-01-29 18:17:15 +0530

    Added route for home page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import FAQ from '../pages/FAQ';
+import HomePage from '../pages/HomePage';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-        <Route path='/' exact />
+        <Route path='/' element={<HomePage />} />
         <Route path='/faq' element={<FAQ />} />
 
       
