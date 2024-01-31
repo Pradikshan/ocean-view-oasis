c1533efbdb17dbeef73933b5bc402ade556813f1
Pradikshan <pradikshan234@gmail.com>
2024-01-31 14:50:08 +0530

    Added new transition duration properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
       },
       backgroundImage: {
         "main-img": "url(../public/images/main_img2.jpg)"
+      },
+      transitionDuration: {
+        "1500": "1500ms",
+        "2000": "2000ms",
+        "2500": "2500ms",
+        "3000": "3000ms",
+        "3500": "3500ms",
+        "4000": "4000ms",
+        "5000": "5000ms"
       }
     },
   },
