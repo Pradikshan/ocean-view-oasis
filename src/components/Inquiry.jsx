4ea478d72a83b0dc649ce61c233f2555ecfd8cb8
Pradikshan <pradikshan234@gmail.com>
2024-02-01 18:19:53 +0530

    Implemented function to clear form input and prevent form default behaviour

--- src/components/Inquiry.jsx
+++ src/components/Inquiry.jsx
@@ -1,10 +1,21 @@
+import { useRef } from "react";
 
 const Inquiry = () => {
+
+    const formRef = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+      
+        formRef.current.reset();
+        alert("Your inquiry was submitted successfully!")
+    }
+
     return (
         <div>
             <p className="text-2xl md:text-3xl font-bold">Inquiry</p>
-            <p className="text-md md:text-xl mb-4">Inquiry</p>
-            <form>
+            <p className="text-sm md:text-md mb-4 overflow-hidden">Send us your questions and inquiries, and we'll be delighted to assist you on your journey to discovering Ocean View Oasis.</p>
+            <form ref={formRef} onSubmit={handleSubmit}>
                 <div className="label-group">
                     <label htmlFor="name" className="contact-label">Name:</label>
                     <input name="name" type="text" className="contact-input"/>
@@ -28,8 +39,8 @@ const Inquiry = () => {
                 <button className="p-2 bg-yellow-700 rounded-full w-36 font-bold text-lg">
                     Submit
                 </button>
-                </form>
-            </div>
+            </form>
+        </div>
     );
 
 }
