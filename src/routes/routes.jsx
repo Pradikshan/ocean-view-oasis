b7c32466087fcb099c5052c4ab31783e8a0c4c23
Pradikshan <pradikshan234@gmail.com>
2024-02-01 17:34:58 +0530

    Added route path for the contact us page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import FAQ from '../pages/FAQ';
 import HomePage from '../pages/HomePage';
+import ContactUsPage from '../pages/ContactUsPage';
 
 export const AppRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/faq' element={<FAQ />} />
-
-      
+        <Route path='/contact_us' element={<ContactUsPage />} />
     </Routes>
   );
 };
