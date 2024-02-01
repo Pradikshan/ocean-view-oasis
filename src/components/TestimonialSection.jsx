cdca808932a5b3610d665679d3428d32f01a09a3
Pradikshan <pradikshan234@gmail.com>
2024-02-01 11:00:45 +0530

    Added logic to play animation when the section is in view

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -5,6 +5,8 @@ import TestimonialCard from "./TestimonialCard";
 
 const TestimonialSection = () => {
 
+    const [ref, inView] = useInView();
+
     const [ref1, inView1] = useInView();
     const [ref2, inView2] = useInView();
     const [ref3, inView3] = useInView();
@@ -82,7 +84,7 @@ const TestimonialSection = () => {
     // };
 
     return (
-        <div className="mt-28">
+        <div className="mt-28 testimonial-section" ref={ref}>
             <div className="flex flex-col justify-center items-center">
                 <p className="text-3xl md:text-4xl font-bold">Testimonials</p>
                 <p className="text-xl md:text-2xl font-semibold">Discover what our residents have to say about Ocean View Oasis.</p>
@@ -93,21 +95,21 @@ const TestimonialSection = () => {
             <div className="grid grid-cols-1 md:grid-cols-3 place-items-stretch">
 
                 <TestimonialCard 
-                className={`${inView1 ? 'show' : ''}`}
+                className={`${inView ? 'show' : ''}`}
                 ref={ref1}
                 image={"../images/gandalf.jpg"}
                 name={"Gandalf the Grey"} 
                 testimonial={"Ocean View Oasis is a marvel! The soothing ocean vistas, spacious interiors, and enchanting atmosphere make it a truly magical retreat. Whether you're a wizard or a mere mortal, this apartment is a haven by the sea."} />
 
                 <TestimonialCard 
-                className={`${inView2 ? 'show' : ''}`}
+                className={`${inView ? 'show' : ''}`}
                 ref={ref2}
                 image={"../images/aragorn5.jpg"}
                 name={"Aragorn, Son of Arathorn"}
                 testimonial={"In my journeys across realms and kingdoms, I have seldom found a residence as splendid as Ocean View Oasis. The majestic views of the vast ocean, the regal interiors, and the serene atmosphere create an abode fit for a king. Whether you seek respite or luxury, this apartment is a testament to the finer things in life."} />
 
                 <TestimonialCard 
-                className={`${inView3 ? 'show' : ''}`}
+                className={`${inView ? 'show' : ''}`}
                 ref={ref3}
                 image={"../images/legolas5.jpg"}
                 name={"Legolas Greenleaf"}
