9fceb1533c366e2c067ccf44c0a5646a6e85321d
Pradikshan <pradikshan234@gmail.com>
2024-02-02 18:44:27 +0530

    Implemented ImageOverlay component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useInView } from 'react-intersection-observer';
 import { ImageCard } from "../components/ImageCard";
 import { SplitLayout } from "../components/SplitLayout";
 import TestimonialSection from "../components/TestimonialSection";
+import { ImageOverlay } from '../components/ImageOverlay';
 
 
 const HomePage = () => {
@@ -45,18 +46,19 @@ const HomePage = () => {
 
     return (
         <>
-        <div className="relative">
+        <ImageOverlay image_path={"../images/main_img2.jpg"} opacity={"opacity-80"} heading={"OCEAN VIEW OASIS"} subheading={"Your Spacious Haven by the Shore"} />
+        {/* <div className="relative">
             <div className="flex flex-col justify-center items-center">
                 <img src="../images/main_img2.jpg" alt="main" className="w-full h-auto opacity-80 z-0" />
                 <div className="z-10 absolute">
                     <div className="flex flex-col justify-center items-center">
                         <p className="font-bold text-lg md:text-4xl text-white ps-3">OCEAN VIEW OASIS</p>
                         <p className="font-lg text-sm md:text-2xl text-white">Your Spacious Haven by the Shore</p>
-                        {/* TODO: add a CTA btn */}
+                    
                     </div>
                 </div>
             </div>
-        </div>
+        </div> */}
 
         <div className={`info-card-container ${inView1 ? 'show' : ''}`} ref={ref1}>
             <SplitLayout>
