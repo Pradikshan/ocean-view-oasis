0bc5369db4f7adb7004262d9fe4559a5df984ab0
Pradikshan <pradikshan234@gmail.com>
2024-02-02 18:44:46 +0530

    Removed commented out code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -47,18 +47,6 @@ const HomePage = () => {
     return (
         <>
         <ImageOverlay image_path={"../images/main_img2.jpg"} opacity={"opacity-80"} heading={"OCEAN VIEW OASIS"} subheading={"Your Spacious Haven by the Shore"} />
-        {/* <div className="relative">
-            <div className="flex flex-col justify-center items-center">
-                <img src="../images/main_img2.jpg" alt="main" className="w-full h-auto opacity-80 z-0" />
-                <div className="z-10 absolute">
-                    <div className="flex flex-col justify-center items-center">
-                        <p className="font-bold text-lg md:text-4xl text-white ps-3">OCEAN VIEW OASIS</p>
-                        <p className="font-lg text-sm md:text-2xl text-white">Your Spacious Haven by the Shore</p>
-                    
-                    </div>
-                </div>
-            </div>
-        </div> */}
 
         <div className={`info-card-container ${inView1 ? 'show' : ''}`} ref={ref1}>
             <SplitLayout>
