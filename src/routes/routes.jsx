c56d2914ff235dc40af7c2cf4cc0edf59cf4a37f
Pradikshan <pradikshan234@gmail.com>
2024-02-02 18:18:03 +0530

    Updated routes file

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import FAQ from '../pages/FAQ';
 import HomePage from '../pages/HomePage';
 import ContactUsPage from '../pages/ContactUsPage';
+import FloorPlanPage from '../pages/FloorplanPage';
 
 export const AppRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/floor_plan' element={<FloorPlanPage />} />
         <Route path='/faq' element={<FAQ />} />
         <Route path='/contact_us' element={<ContactUsPage />} />
     </Routes>
