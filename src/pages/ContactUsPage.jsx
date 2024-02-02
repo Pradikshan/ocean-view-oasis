a7efbb0f43b75656ee1b1b7c0152bbfd1285e34b
Pradikshan <pradikshan234@gmail.com>
2024-02-02 18:42:54 +0530

    Removed commented out code

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -10,17 +10,6 @@ const ContactUsPage = () => {
         image_path={"../images/service.jpg"} 
         opacity={"opacity-90"} heading={"Contact Us"} 
         subheading={"Reach out to us – let's start a conversation about turning your dreams of coastal living into a reality"} />
-        {/* <div className="relative">
-            <div className="flex flex-col justify-center items-center">
-                <img src="../images/service.jpg" alt="customer_service" className="z-0 opacity-95 w-full h-1/2" />
-                <div className="z-10 absolute">
-                    <div className="flex flex-col justify-center items-center">
-                        <p className="font-bold text-lg md:text-4xl text-white ps-3">Contact Us</p>
-                        <p className="font-lg text-sm md:text-2xl text-center text-white">Reach out to us – let's start a conversation about turning your dreams of coastal living into a reality</p>
-                    </div>
-                </div> 
-            </div>
-        </div> */}
 
         <div className="pt-8 md:pt-16">
             <SplitLayout>
