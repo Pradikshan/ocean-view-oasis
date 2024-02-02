50ac670477aaa88944473dc26a2ded62f5c61ce0
Pradikshan <pradikshan234@gmail.com>
2024-02-02 18:47:27 +0530

    Made minor changes - indentation

--- src/pages/FloorPlanPage.jsx
+++ src/pages/FloorPlanPage.jsx
@@ -4,7 +4,11 @@ import { ImageOverlay } from "../components/ImageOverlay";
 const FloorPlanPage = () => {
     return (
         <>
-        <ImageOverlay image_path={"../images/plan2.jpg"} opacity={"opacity-80"} heading={"Floor Plan"} subheading={"Explore our meticulously designed floor plans, tailored for your lifestyle and comfort"} />
+        <ImageOverlay 
+        image_path={"../images/plan2.jpg"} 
+        opacity={"opacity-80"} 
+        heading={"Floor Plan"} 
+        subheading={"Explore our meticulously designed floor plans, tailored for your lifestyle and comfort"} />
         </>
     );
 }
