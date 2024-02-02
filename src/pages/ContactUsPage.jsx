b017fad3442f490e74869de7fcd30d2a52f56fa2
Pradikshan <pradikshan234@gmail.com>
2024-02-02 18:42:23 +0530

    Replaced image with the ImageOverlay component

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -1,22 +1,26 @@
 import { SplitLayout } from "../components/SplitLayout";
 import Inquiry from "../components/Inquiry";
+import { ImageOverlay } from "../components/ImageOverlay";
 
 
 const ContactUsPage = () => {
     return (
         <>
-        <div className="relative">
+        <ImageOverlay 
+        image_path={"../images/service.jpg"} 
+        opacity={"opacity-40"} heading={"Contact Us"} 
+        subheading={"Reach out to us – let's start a conversation about turning your dreams of coastal living into a reality"} />
+        {/* <div className="relative">
             <div className="flex flex-col justify-center items-center">
                 <img src="../images/service.jpg" alt="customer_service" className="z-0 opacity-95 w-full h-1/2" />
                 <div className="z-10 absolute">
                     <div className="flex flex-col justify-center items-center">
                         <p className="font-bold text-lg md:text-4xl text-white ps-3">Contact Us</p>
                         <p className="font-lg text-sm md:text-2xl text-center text-white">Reach out to us – let's start a conversation about turning your dreams of coastal living into a reality</p>
-                        {/* TODO: add a CTA btn */}
                     </div>
                 </div> 
             </div>
-        </div>
+        </div> */}
 
         <div className="pt-8 md:pt-16">
             <SplitLayout>
