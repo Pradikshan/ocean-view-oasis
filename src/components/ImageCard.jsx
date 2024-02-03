5024d0988fe759897dfc3e1a7d0d177aaad3b437
Pradikshan <pradikshan234@gmail.com>
2024-02-03 12:40:42 +0530

    Added logic to pass rounded value as props

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,8 +1,8 @@
 
-const ImageCard = ({ image_path }) => {
+const ImageCard = ({ image_path, rounded }) => {
     return (
         <div className="basis-full md:basis-5/6">
-            <img src={image_path} alt="info" className="rounded-3xl h-full w-full object-cover" />
+            <img src={image_path} alt="info" className={`${rounded} h-full w-full object-cover`} />
         </div>
     );
 }
