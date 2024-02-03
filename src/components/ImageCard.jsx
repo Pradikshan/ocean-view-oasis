0a803e044c1e4792e8ff1c5b78550302caabb77f
Pradikshan <pradikshan234@gmail.com>
2024-02-03 11:51:34 +0530

    Changed from named export to default export

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,7 +1,10 @@
-export const ImageCard = ({ image_path }) => {
+
+const ImageCard = ({ image_path }) => {
     return (
         <div className="basis-full md:basis-5/6">
             <img src={image_path} alt="info" className="rounded-3xl h-full w-full object-cover" />
         </div>
     );
 }
+
+export default ImageCard;
