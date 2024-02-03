2de1a02379f4f90a4e5389119a7d49a7bc84d2a9
Pradikshan <pradikshan234@gmail.com>
2024-02-03 13:11:02 +0530

    Made the layout responsive

--- src/components/IconText.jsx
+++ src/components/IconText.jsx
@@ -1,6 +1,6 @@
 const IconText = ({ icon, text }) => {
     return (
-        <div className="flex flex-row mt-1">
+        <div className="flex flex-row mt-3 md:mt-1">
             <div className="text-2xl items-center pe-1">
                 {icon}
             </div>
