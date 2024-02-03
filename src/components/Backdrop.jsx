f046d6caa8bbedaf0217d6f32ea51813f069eb1f
Pradikshan <pradikshan234@gmail.com>
2024-02-03 22:10:33 +0530

    Implemented Backdrop component

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+
+const Backdrop = ({ children, onClick }) => {
+    return (
+        <motion.div 
+        className="absolute top-0 left-0 w-full h-full flex justify-center items-center transparent bg-trans"
+        onClick={onClick}
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1}}
+        exit={{ opacity: 0}}
+        >
+            {children}
+        </motion.div>
+    )
+}
+
+export default Backdrop;
