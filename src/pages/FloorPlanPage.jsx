d1360c6f33570e4b66b46f0ab2213e6c1ff978d9
Pradikshan <pradikshan234@gmail.com>
2024-02-03 11:58:16 +0530

    Imported IconText component

--- src/pages/FloorPlanPage.jsx
+++ src/pages/FloorPlanPage.jsx
@@ -9,6 +9,23 @@ const FloorPlanPage = () => {
         opacity={"opacity-90"} 
         heading={"Floor Plan"} 
         subheading={"Explore our meticulously designed floor plans, tailored for your lifestyle and comfort"} />
+
+        <div className="m-10 rounded-xl shadow-xl">
+            <div className="flex flex-row p-8">
+                <div className="basis-1/2 border-2 border-black">
+                    <img src="../images/floor.png" alt="floor plan" className="w-full h-full object-cover" />
+                </div>
+
+                <div className="flex flex-col ms-8">
+                    <p className="text-2xl font-bold">Apartment floor plan</p>
+                    <p>Uncover the intricacies of our apartment floor plan, revealing a blueprint that hosts five thoughtfully designed living spaces on each floor, harmonizing comfort and style seamlessly</p>
+                    <p className="text-xl font-bold mt-4">Key features</p>
+                </div>
+            </div>
+
+
+
+        </div>
         </>
     );
 }
