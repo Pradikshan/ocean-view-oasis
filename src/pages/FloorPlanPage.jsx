354152fa79187f7e18da84554995c34bb4addef6
Pradikshan <pradikshan234@gmail.com>
2024-02-03 13:04:53 +0530

    Implemented code for the 1-bedroom floor plan

--- src/pages/FloorPlanPage.jsx
+++ src/pages/FloorPlanPage.jsx
@@ -54,6 +54,42 @@ const FloorPlanPage = () => {
             </div>
         </div>
 
+        <div className="m-10 rounded-xl shadow-xl">
+            <div className="flex flex-row p-8">
+                <ImageCard image_path={"../images/bed1.png"} />
+
+                <div className="flex flex-col ms-8">
+                    <p className="text-2xl font-bold">1-Bedroom Living at Its Finest</p>
+                    <p>Explore our 1-bedroom floor plan – a harmonious blend of thoughtful design, spacious living areas, modern kitchens, and a well-appointed bedroom – creating an inviting and cozy living space for you</p>
+                    <p className="text-xl font-bold mt-4">Key features</p>
+                    <IconText 
+                    icon={<MdOutlineBedroomParent />} 
+                    text={"A comfortable and well-designed bedroom space offering a private retreat for relaxation"} />
+                    <IconText 
+                    icon={<MdOutlineKitchen />} 
+                    text={"A modern kitchen layout with high-end appliances"} />
+                    <IconText 
+                    icon={<MdOutlineBathroom />} 
+                    text={"Thoughtfully crafted bathroom design, balancing elegance with functionality"} />
+                    <IconText 
+                    icon={<MdOutlineBalcony />} 
+                    text={"Enjoy a personal balcony, providing a secluded escape"} />
+                    <IconText 
+                    icon={<MdOutlineLocalLaundryService />} 
+                    text={"In-unit laundry facilities for convinience"} />
+                    <IconText 
+                    icon={<MdOutlineChair />} 
+                    text={"Embrace the flexibility of an open floor plan, allowing you to personalize your living space"} />
+                    <IconText 
+                    icon={<MdOutlineHeatPump />} 
+                    text={"Year round comfort with energy-efficient heating and cooling systems"} />
+                    <IconText 
+                    icon={<BiCctv />} 
+                    text={"Feel secure with controlled access and 24/7 surveillance"} />
+                </div>
+            </div>
+        </div>
+
         
         </>
     );
