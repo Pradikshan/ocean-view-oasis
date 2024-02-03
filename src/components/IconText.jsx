a72e09eae83eba9c492eccf87cf184c9e600a84b
Pradikshan <pradikshan234@gmail.com>
2024-02-03 12:39:24 +0530

    Added layout and adjusted spacing

--- src/components/IconText.jsx
+++ src/components/IconText.jsx
@@ -1,8 +1,12 @@
 const IconText = ({ icon, text }) => {
     return (
-        <div className="flex flex-row">
-            {icon}
-            {text}
+        <div className="flex flex-row mt-1">
+            <div className="text-2xl items-center pe-1">
+                {icon}
+            </div>
+            <div>
+                {text}
+            </div>
         </div>
     );
 }
