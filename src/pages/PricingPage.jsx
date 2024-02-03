1c0a8bcf3c4b7dc18cdea6168bb270fb4edc26c0
Pradikshan <pradikshan234@gmail.com>
2024-02-03 22:11:05 +0530

    Testing the modal component

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,6 +1,32 @@
-const PricingPage = () => {
-    return (
+import { motion } from "framer-motion";
+import { useState } from "react";
+import Modal from "../components/Modal";
 
+const PricingPage = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+   
+
+    const closeModal = () => setIsModalOpen(false);
+    
+
+    console.log(isModalOpen);
+
+    return (
+        <>
+        <motion.button 
+        className="bg-black text-white" 
+        onClick={() => (isModalOpen ? closeModal() : openModal())}
+        whileHover={{ scale: 2.1}}
+        whileTap={{ scale: 0.9}}>
+            Click me</motion.button>
+
+        {isModalOpen && <Modal isModalOpen={isModalOpen} handleClose={closeModal} text={"asjjhask"}/>}
+        </>
+
+        
     );
 }
 
