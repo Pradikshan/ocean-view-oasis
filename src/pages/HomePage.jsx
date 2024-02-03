4003bc3ea4a58dd8054eac2ce4bf1421fdd1f64c
Pradikshan <pradikshan234@gmail.com>
2024-02-03 11:55:04 +0530

    Changed ImageOverlay import syntax

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { useInView } from 'react-intersection-observer';
 import ImageCard from "../components/ImageCard";
 import SplitLayout from "../components/SplitLayout";
 import TestimonialSection from "../components/TestimonialSection";
-import { ImageOverlay } from '../components/ImageOverlay';
+import ImageOverlay from '../components/ImageOverlay';
 
 
 const HomePage = () => {
