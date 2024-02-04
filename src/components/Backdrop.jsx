8f71a8f1b6bc5487c33b0f9c382017b99009cdd1
Pradikshan <pradikshan234@gmail.com>
2024-02-04 11:53:46 +0530

    Added a key value

--- src/components/Backdrop.jsx
+++ src/components/Backdrop.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 const Backdrop = ({ children, onClick }) => {
     return (
         <motion.div 
+        key="backdrop"
         className="absolute top-0 left-0 w-full h-full flex justify-center items-center transparent bg-trans"
         onClick={onClick}
         initial={{ opacity: 0}}
