2190c54e1702e0e11618342efefbfc204023296f
Pradikshan <pradikshan234@gmail.com>
2024-02-04 19:12:02 +0530

    Implemented custom useFetch hook

---
+++ src/utils/hooks/useFetch.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+
+export const useFetch = (path) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch(path);
+                const data = await response.json();
+                setData(data);
+            } catch (error) {
+                setError(error);
+            } 
+        };
+
+        fetchData();
+    }, [path]);
+}
