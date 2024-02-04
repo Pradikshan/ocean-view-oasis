15f98d923539c52f0e527243f8bf158850fc8dad
Pradikshan <pradikshan234@gmail.com>
2024-02-04 17:59:32 +0530

    Changed h value for the image

--- src/components/ImageOverlay.jsx
+++ src/components/ImageOverlay.jsx
@@ -2,7 +2,7 @@ const ImageOverlay = ({ image_path, opacity, heading, subheading }) => {
     return (
         <div className="relative">
             <div className="flex flex-col justify-center items-center">
-                <img src={`${image_path}`} alt="main" className={`w-full h-auto ${opacity} z-0`} />
+                <img src={`${image_path}`} alt="main" className={`w-full h-1/12 ${opacity} z-0`} />
                 <div className="z-10 absolute">
                     <div className="flex flex-col justify-center items-center">
                         <p className="font-bold text-lg md:text-4xl text-white ps-3">{heading}</p>
