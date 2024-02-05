0724db88081faa2a5e1ee19a25412c570c734406
Pradikshan <pradikshan234@gmail.com>
2024-02-05 17:25:51 +0530

    Removed mapping function

--- src/components/IconText.jsx
+++ src/components/IconText.jsx
@@ -1,22 +1,10 @@
-import { MdOutlineLiving, MdOutlineKitchen, MdOutlineBedroomParent, MdOutlineBathroom, MdOutlineBalcony, MdOutlineLocalLaundryService, MdOutlineChair, MdOutlineHeatPump, MdOutlineCleaningServices, MdOutlineSpaceDashboard, MdSpaceDashboard, MdOutlineAccountBalanceWallet, MdOutlineEventAvailable } from "react-icons/md";
-import { FaRegMoneyBillAlt } from "react-icons/fa";
-import { LiaFileContractSolid } from "react-icons/lia";
-
-
-const iconMapping = {
-    '<FaRegMoneyBillAlt />': <FaRegMoneyBillAlt />,
-    '<LiaFileContractSolid />': <LiaFileContractSolid />,
-    // Add mappings for other icons
-  };
-
 const IconText = ({ icon, text }) => {
 
-    const iconComponent = iconMapping[icon];
 
     return (
         <div className="flex flex-row mt-3 md:mt-1">
             <div className="text-2xl items-center pe-1">
-                {iconComponent}
+                {icon}
             </div>
             <div>
                 {text}
