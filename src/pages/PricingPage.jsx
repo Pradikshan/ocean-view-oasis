4027c5b747fddd81a0f43156cd7f8d5f9ca87354
Pradikshan <pradikshan234@gmail.com>
2024-02-05 17:24:26 +0530

    Added code to map the data

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,10 +1,13 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { useFetch } from "../utils/hooks/useFetch";
 import Modal from "../components/Modal";
 import PricingCard from "../components/PricingCard";
+
 import { MdOutlineLiving, MdOutlineKitchen, MdOutlineBedroomParent, MdOutlineBathroom, MdOutlineBalcony, MdOutlineLocalLaundryService, MdOutlineChair, MdOutlineHeatPump, MdOutlineCleaningServices, MdOutlineSpaceDashboard, MdSpaceDashboard, MdOutlineAccountBalanceWallet, MdOutlineEventAvailable } from "react-icons/md";
 import { FaRegMoneyBillAlt } from "react-icons/fa";
 import { LiaFileContractSolid } from "react-icons/lia";
+import IconText from "../components/IconText";
 
 const PricingPage = () => {
 
@@ -16,17 +19,44 @@ const PricingPage = () => {
     const closeModal = () => setIsModalOpen(false);
     
 
-    console.log(isModalOpen);
+    // console.log(isModalOpen);
 
+    const {data, loading, error } = useFetch("/assets/pricing.json");
+
+    if (loading) return <p>Loading...</p>
+
+    if (error) return <p>Error...</p>
+
+    if (!data) return null;
+
+   
+    // return (
+        
+    //     // data.map((category) => (
+    //     //     <p>{category.categoryId}</p>
+    //     // ))
+    // );
+
+
+    
+
+
+    
     return (
         <>
         <div>
             <p className="text-center text-3xl font-bold mt-4">Renting options</p>
             <div className="flex flex-row">
-                <PricingCard />
-                <PricingCard />
-                <PricingCard />
-                <PricingCard />
+                {data
+                .filter((category) => category.categoryId === 1)
+                .flatMap((category) => 
+                category.options.map((apartment) => (
+                    <>
+                    <PricingCard image={apartment.image} title={apartment.title} />
+                    <IconText icon={apartment.info.icon} text={apartment.info.text} />
+                    </>
+                )     
+                ))}  
             </div>
         </div>
 
