6c18c5ebf83ec46b779abbc861bf1d058fd0bc4b
Pradikshan <pradikshan234@gmail.com>
2024-02-07 16:46:37 +0530

    Renamed from FAQ to FAQPage

---
+++ src/pages/FAQPage.jsx
@@ -0,0 +1,36 @@
+
+const FAQPage = () => {
+    return (
+        <>
+        <div className="">
+            <div className="main-container  bg-blue-500">
+                <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+
+
+                <div className="w-3/4 h-3/4 mini-container  bg-rose-500">
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
+                </div>
+        </div>
+        </div>
+
+        
+
+        </>
+    );
+}
+
+export default FAQPage;
