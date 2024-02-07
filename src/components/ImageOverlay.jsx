d9c365ff1de1b4eba8be699529c678a999debb50
Pradikshan <pradikshan234@gmail.com>
2024-02-07 16:08:37 +0530

    Replaced margin-top value with prop

--- src/components/ImageOverlay.jsx
+++ src/components/ImageOverlay.jsx
@@ -4,7 +4,7 @@ const ImageOverlay = ({ image_path, opacity, heading, subheading, margin_top }) 
             <div className="flex flex-col justify-center items-center">
                 <img src={`${image_path}`} alt="main" className={`w-full h-1/12 ${opacity} z-0`} />
                 <div className="z-10 absolute">
-                    <div className="flex flex-col justify-center items-center -mt-28">
+                    <div className={`flex flex-col justify-center items-center ${margin_top}`}>
                         <p className="font-bold text-lg md:text-4xl text-white md:ps-3">{heading}</p>
                         <p className="font-lg text-sm md:text-2xl text-white">{subheading}</p>
                         {/* TODO: add a CTA btn */}
