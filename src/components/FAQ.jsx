2aa07dd9f1b954ca148dcdb3d574c27623afc3ca
Pradikshan <pradikshan234@gmail.com>
2024-02-07 18:14:32 +0530

    Imported plus icon

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,6 +1,13 @@
+import { FaPlus } from "react-icons/fa6";
+
 const FAQ = ({ question, answer }) => {
     return (
+        <div>
+            <div className="flex flex-row">
+                <p>{question}</p>
 
+            </div>
+        </div>
     );
 }
 
