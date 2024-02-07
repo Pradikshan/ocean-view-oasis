7cc98b4e64ba7017771188df577fc149e795adb4
Pradikshan <pradikshan234@gmail.com>
2024-02-07 16:46:24 +0530

    Updated FAQ route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import FAQ from '../pages/FAQ';
+import FAQPage from '../pages/FAQPage';
 import HomePage from '../pages/HomePage';
 import ContactUsPage from '../pages/ContactUsPage';
 import FloorPlanPage from '../pages/FloorPlanPage';
@@ -12,7 +12,7 @@ export const AppRoutes = () => {
         <Route path='/' element={<HomePage />} />
         <Route path='/floor_plan' element={<FloorPlanPage />} />
         <Route path='/pricing' element={<PricingPage />} />
-        <Route path='/faq' element={<FAQ />} />
+        <Route path='/faq' element={<FAQPage />} />
         <Route path='/contact_us' element={<ContactUsPage />} />
     </Routes>
   );
