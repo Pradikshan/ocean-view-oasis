f19a988842fef2facf52e08f2b3a7109561988b8
Pradikshan <pradikshan234@gmail.com>
2024-02-07 17:49:28 +0530

    Implemented Badge component

---
+++ src/components/Badge.jsx
@@ -0,0 +1,9 @@
+const Badge = ({ text }) => {
+    return (
+        <div className="w-56 rounded-full p-3 m-3 shadow-2xl bg-slate-600">
+            <p className="text-center text-white text-lg font-bold">{text}</p>
+        </div>
+    );
+}
+
+export default Badge;
