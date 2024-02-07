d289e71765b7531a73f042ad5151c8fa1f762e53
Pradikshan <pradikshan234@gmail.com>
2024-02-07 15:53:01 +0530

    Adjusted padding of the heading

--- src/components/ImageOverlay.jsx
+++ src/components/ImageOverlay.jsx
@@ -4,8 +4,8 @@ const ImageOverlay = ({ image_path, opacity, heading, subheading }) => {
             <div className="flex flex-col justify-center items-center">
                 <img src={`${image_path}`} alt="main" className={`w-full h-1/12 ${opacity} z-0`} />
                 <div className="z-10 absolute">
-                    <div className="flex flex-col justify-center items-center -m-28">
-                        <p className="font-bold text-lg md:text-4xl text-white ps-3">{heading}</p>
+                    <div className="flex flex-col justify-center items-center -mt-28">
+                        <p className="font-bold text-lg md:text-4xl text-white md:ps-3">{heading}</p>
                         <p className="font-lg text-sm md:text-2xl text-white">{subheading}</p>
                         {/* TODO: add a CTA btn */}
                     </div>
