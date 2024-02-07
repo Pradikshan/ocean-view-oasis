a850fa769abfdbc35d5744f4d8166b70e5a3da51
Pradikshan <pradikshan234@gmail.com>
2024-02-07 19:57:22 +0530

    Removed subheading

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -6,7 +6,7 @@ const FAQPage = () => {
         <>
         <div className="mt-16">
            <p className="text-3xl font-bold text-center">FAQ</p>
-           <p className="text-xl font-bold text-center">We have got answers</p>
+           {/* <p className="text-xl font-bold text-center">We have got answers</p> */}
         </div>
 
         <div className="flex flex-row justify-center">
