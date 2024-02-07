3f26d5de2cfe0abfb3036f6fe81e3dc96756a873
Pradikshan <pradikshan234@gmail.com>
2024-02-07 16:56:33 +0530

    Removed old code

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -2,29 +2,8 @@
 const FAQPage = () => {
     return (
         <>
-        <div className="">
-            <div className="main-container  bg-blue-500">
-                <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-
-
-                <div className="w-3/4 h-3/4 mini-container  bg-rose-500">
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                    <p className="text-center">hsjhshjsjhahajhjsahjashajs</p>
-                </div>
-        </div>
+        <div>
+           
         </div>
 
         
