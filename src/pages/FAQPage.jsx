f14408223ac612b9e9925b323807b38849375ced
Pradikshan <pradikshan234@gmail.com>
2024-02-07 17:49:37 +0530

    Imported Badge component

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -1,9 +1,15 @@
+import Badge from "../components/Badge";
 
 const FAQPage = () => {
     return (
         <>
-        <div>
-           
+        <div className="mt-16">
+           <p className="text-2xl font-bold text-center">FAQ</p>
+           <p className="text-xl font-bold text-center">We have got answers</p>
+        </div>
+
+        <div className="flex flex-row justify-center">
+            <Badge text={"Service"} />
         </div>
 
         
