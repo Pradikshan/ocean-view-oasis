0171ad7e4ef98eda73317cc72915d1ec6c232aa2
Pradikshan <pradikshan234@gmail.com>
2024-02-07 18:17:14 +0530

    Added font styling

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,11 +1,14 @@
 import { FaPlus } from "react-icons/fa6";
 
 const FAQ = ({ question, answer }) => {
+
+    const style ={fontSize: 30};
+
     return (
         <div>
-            <div className="flex flex-row">
-                <p>{question}</p>
-
+            <div className="flex flex-row justify-between mx-2">
+                <p className="text-xl font-bold">{question}</p>
+                <FaPlus style={style}/>
             </div>
         </div>
     );
