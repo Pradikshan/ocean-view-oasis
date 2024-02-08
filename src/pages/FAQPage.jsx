2899f993c93eff12643138020f8f7e70476a597b
Pradikshan <pradikshan234@gmail.com>
2024-02-08 10:22:19 +0530

    Made the layout responsive

--- src/pages/FAQPage.jsx
+++ src/pages/FAQPage.jsx
@@ -4,12 +4,12 @@ import FAQ from "../components/FAQ";
 const FAQPage = () => {
     return (
         <>
-        <div className="mt-10">
+        <div className="mt-5">
            <p className="text-3xl font-bold text-center">FAQ</p>
            {/* <p className="text-xl font-bold text-center">We have got answers</p> */}
         </div>
 
-        <div className="flex flex-row justify-center">
+        <div className="flex flex-col md:flex-row items-center justify-center">
             <Badge text={"Apartment"} />
             <Badge text={"Customer service"} />
             <Badge text={"Buying/Renting"} />
