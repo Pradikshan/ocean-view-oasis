20d67b50f3f635c5d826e5b26522b0848dee04a2
Pradikshan <pradikshan234@gmail.com>
2024-02-08 12:08:04 +0530

    Added animations

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,24 +1,52 @@
 import { useState } from "react";
 import { FaPlus, FaMinus } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 const FAQ = ({ question, answer }) => {
 
     const [selected, setSelected] = useState(false);
 
-    const handleClick = () => {
+    const handleClick = (e) => {
+        e.stopPropagation();
         setSelected(!selected);
         console.log(selected);
     }
 
     const style ={fontSize: 25};
 
+    const fadeIn = {
+        hidden: {
+            opacity: 0,
+        },
+        visibile: {
+            opacity: 1,
+            transition: {
+                duration: 1,
+            },
+        },
+        exit: {
+            opacity: 0
+        }
+    }
+
     return (
         <div className="mx-2 my-5 cursor-pointer border-2 border-white border-b-slate-600" onClick={handleClick}>
             <div className="flex flex-row justify-between my-2">
                 <p className="text-xl font-bold">{question}</p>
-                {selected ? <FaMinus style={style}/> : <FaPlus style={style}/>}
+                {selected ? 
+                <FaMinus style={style}/> 
+                : <FaPlus style={style}/>}
             </div>
-            { selected ? <p className="-mt-1 text-lg">{answer}</p> : null}
+            { selected ? 
+            <motion.p 
+            className="-mt-1 text-lg"
+            variants={fadeIn}
+            initial="hidden"
+            animate="visibile"
+            exit="exit">
+                {answer}
+            </motion.p> 
+            : null}
         </div>
     );
 }
