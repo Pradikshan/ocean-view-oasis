1d867568ec3f4e7f044c6eff456af4ac9c1b3b35
Pradikshan <pradikshan234@gmail.com>
2024-02-08 20:58:02 +0530

    Added title and paragraph

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -2,7 +2,14 @@
 
 const AboutUsPage = () => {
     return (
-        <></>
+        <>
+        <div className="flex flex-row">
+            <div className="flex flex-col basis-1/2">
+                <p className="text-3xl font-bold">Transforming Living Spaces: Redefining Comfort and Community</p>
+                <p>Discover a new era of living at our apartment community, where innovation intertwines seamlessly with comfort. We're dedicated to shaping spaces that inspire connection, offering residents a haven where modern amenities and a vibrant atmosphere converge to create unforgettable experiences.</p>
+            </div>
+        </div>
+        </>
     );
 
 }
