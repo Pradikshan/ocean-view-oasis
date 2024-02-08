cb2b10204b451374064e189536af340bab8e775b
Pradikshan <pradikshan234@gmail.com>
2024-02-08 19:27:32 +0530

    Changed the layout of the TestimonialCard

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,10 +1,24 @@
-const TestimonialCard = ({ className, image, name, testimonial }) => {
+const TestimonialCard = ({ className, image, name, title, testimonial }) => {
     return (
+        // <div className={`testimonial-card ${className}`}>
+        //     <div className="flex flex-col justify-center items-center px-8 py-3">
+        //         <img src={image} alt="resident" className="rounded-full object-cover w-24 h-24" />
+        //         <p className="text-xl font-bold">{name}</p>
+        //         <p className="pt-4">{testimonial}</p>
+        //     </div>
+        // </div>
+
         <div className={`testimonial-card ${className}`}>
-            <div className="flex flex-col justify-center items-center px-8 py-3">
-                <img src={image} alt="resident" className="rounded-full object-cover w-24 h-24" />
-                <p className="text-xl font-bold">{name}</p>
+            <div className="px-8 py-3">
                 <p className="pt-4">{testimonial}</p>
+                <div className="flex flex-row mt-8">
+                    <img src={image} alt="resident" className="rounded-full object-cover w-16 h-16" />
+                    <div className="flex flex-col ms-5">
+                        <p className="text-xl font-bold">{name}</p>
+                        <p className="">Wizard of Skyrim</p>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     );
