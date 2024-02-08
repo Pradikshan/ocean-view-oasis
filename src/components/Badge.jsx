086d8e9bcdcc72d6fae88584f1404416cdbbd9a1
Pradikshan <pradikshan234@gmail.com>
2024-02-08 10:21:56 +0530

    Adjusted the width

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,6 +1,6 @@
 const Badge = ({ text }) => {
     return (
-        <div className="w-56 rounded-full p-3 m-3 shadow-2xl bg-slate-600">
+        <div className="w-48 rounded-full p-3 m-3 shadow-2xl bg-slate-600">
             <p className="text-center text-white text-lg font-bold">{text}</p>
         </div>
     );
