03ecdfd8bbe8891f5f0172dd35084e8f4321adca
Pradikshan <pradikshan234@gmail.com>
2024-02-08 19:38:33 +0530

    Made minor changes

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -12,12 +12,11 @@ const TestimonialCard = ({ className, image, name, title, testimonial }) => {
             <div className="px-8 py-3">
                 <p className="pt-4">{testimonial}</p>
                 <div className="flex flex-row mt-8">
-                    <img src={image} alt="resident" className="rounded-full object-cover w-16 h-16" />
+                    <img src={image} alt="resident" className="rounded-full object-cover w-12 h-12" />
                     <div className="flex flex-col ms-5">
                         <p className="text-xl font-bold">{name}</p>
                         <p className="">Wizard of Skyrim</p>
-                    </div>
-                    
+                    </div> 
                 </div>
             </div>
         </div>
