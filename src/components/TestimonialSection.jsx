7fbc2f0c19d920dec7d255d984530bf8ea10557c
Pradikshan <pradikshan234@gmail.com>
2024-02-08 20:20:27 +0530

    Added three testimonials

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -169,18 +169,41 @@ const TestimonialSection = () => {
             testimonial={"Ocean View Oasis, where sea meets stone, is a dwelling that befits both dwarf and elf alike. The sturdy interiors, reminiscent of the caverns of Khazad-dûm, provide a sense of security and comfort. It stands as a testament to the unity of sea and mountain, offering a haven to revel in both the vastness of the ocean and the enduring strength of the stone. A home worthy of any stout-hearted dweller."} />
 
             <TestimonialCard
+            className={"h-full"}
             image={"../images/merry.jpg"}
             name={"Merry Brandybuck"}
             title={"Master of Buckland"}
             testimonial={"Ocean View Oasis echoes the warmth of the Shire. The cozy interiors, reminiscent of a Hobbit hole, offer unmatched comfort. It stands as a testament to the welcoming embrace of sea and home, a haven where one can enjoy the tranquility of the ocean and the camaraderie of the hearth. A Hobbit's perfect retreat."} />
 
             <TestimonialCard
-            className={"h-full"}
             image={"../images/pippin.jpg"}
             name={"Peregin Took"}
             title={"Knight of Gondor"}
             testimonial={"Ocean View Oasis is a merry abode where the sea's tales mingle with the laughter of friends. The lively interiors, like a Hobbit's haven, exude joy and comfort. A testament to the perfect blend of sea and camaraderie, it offers a haven where one can revel in the ocean's melody and the cheer of good company. A home that sparks joy in every Hobbit's heart."} />
+
+            <TestimonialCard
+            className={"h-fit"}
+            image={"../images/sam.jpg"}
+            name={"Samwise Gamgee"}
+            title={"The faithful friend"}
+            testimonial={"Ocean View Oasis is more than just a place; it's a haven where the sea's whispers mingle with the warmth of home. Every moment spent here feels like a gentle embrace from the Shire itself."} />
+
+            <TestimonialCard
+            image={"../images/frodo.jpg"}
+            name={"Frodo Baggins"}
+            title={"The Ring-bearer"}
+            testimonial={"Nestled by the ocean's edge, this haven offered me solace and serenity during my travels. The tranquil ambiance, combined with the gentle lullaby of the waves, provided the perfect backdrop for reflection and rejuvenation. With its cozy interiors and picturesque views, Ocean View Oasis felt like a haven in Middle-earth. I couldn't have asked for a more peaceful retreat."} />
+
+            <TestimonialCard
+            className={"col-span-2"}
+            content_style={"text-xl font-bold"}
+            image={"../images/sauron.jpg"}
+            name={"Sauron the Deceiver"}
+            title={"Dark lord of Mordor"}
+            testimonial={"As the Dark Lord of Mordor, I can appreciate the finer things in life, and Ocean View Oasis does not disappoint. The breathtaking vistas of the sea remind me of the power and vastness of my domain. The luxurious interiors offer a taste of opulence fit for a ruler of Middle-earth. Ocean View Oasis is truly a haven worthy of one who seeks dominion over land and sea."} />
             </div>
+
+            
 
         
         </div>
