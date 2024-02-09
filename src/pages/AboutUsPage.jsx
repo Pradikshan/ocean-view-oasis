1d131677d3991b127a7ec4c7bc93add0cb40bbd5
Pradikshan <pradikshan234@gmail.com>
2024-02-09 21:35:05 +0530

    Changed conditional rendering of image tiles

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -9,14 +9,13 @@ const AboutUsPage = () => {
         <>
         <div className="mt-4 md:mt-28">
             
-            <div className="flex flex-col md:flex-row">
+            <div className="flex flex-col md:flex-row relative">
                 <div className="flex flex-col md:w-1/2 py-12 md:py-72 px-5 md:px-12">
                     <p className="text-2xl md:text-3xl font-bold">Transforming Living Spaces: Redefining Comfort and Community</p>
                     <p>Discover a new era of living at our apartment community, where innovation intertwines seamlessly with comfort. We're dedicated to shaping spaces that inspire connection, offering residents a haven where modern amenities and a vibrant atmosphere converge to create unforgettable experiences.</p>
                 </div>
+                {window.innerWidth >= 768 ? (
                 <div className="relative md:w-1/2">
-                    {window.innerWidth >= 768 ? (
-                    <>
                     <div className="absolute md:top-48 md:left-0">
                         <ImageTile image_path={"../images/tile3-min.jpg"} />
                     </div>
@@ -24,10 +23,6 @@ const AboutUsPage = () => {
                     <div className="absolute md:bottom-0 md:left-56">
                         <ImageTile image_path={"../images/tile2-min.jpg"} />
                     </div>
-                    </>
-                    )
-                    : null
-                    }
 
                     <div className="absolute -top-5 -left-20 md:top-20 md:left-56">
                         <ImageTile image_path={"../images/tile-min.jpg"} />
@@ -37,22 +32,22 @@ const AboutUsPage = () => {
                         <ImageTile image_path={"../images/tile4-min.jpg"} />
                     </div> 
 
-                    {window.innerWidth >= 768 ? (
-                    <>
                     <div className="absolute md:top-96 md:left-[448px]">
                         <ImageTile image_path={"../images/tile5.jpg"} />
-                    </div> 
-                    </>
-                    )
-                    : null
-                    }
-
-                    
+                    </div>                     
                 </div>
+                 )
+                 : null }
+
+
+
+                
+                 
+                  
             </div>
             
 
-            <div className="flex flex-col md:flex-row px-5 md:px-12 mt-28">
+            <div className="flex flex-col md:flex-row px-5 md:px-12 mt-10 md:mt-28">
                 <div className="flex flex-col md:w-1/2">
                     <p className="text-2xl md:text-3xl font-bold">Our Mission</p>
                     <p className="md:text-lg mt-2">At Ocean View Oasis, our mission is to provide a sanctuary by the sea, where residents can embrace the beauty of coastal living while enjoying modern comforts and fostering a strong sense of community. We are committed to creating an environment that promotes well-being, connection, and sustainable living.</p>
