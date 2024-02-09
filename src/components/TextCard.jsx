b306960c6bf5edd06c080ea27d1aafc474961b63
Pradikshan <pradikshan234@gmail.com>
2024-02-09 21:28:01 +0530

    Made the layout responsive

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -1,6 +1,6 @@
 const TextCard = ({ heading, text }) => {
     return (
-        <div className="w-96 my-10">
+        <div className="w-full md:w-96 my-4 md:my-10">
             <p className="text-xl font-bold">{heading}</p>
             <p className="mt-2">{text}</p>
         </div>
