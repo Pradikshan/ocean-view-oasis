3eba51a482d27e5e72e8dd0dc7530dc024e3fca6
Pradikshan <pradikshan234@gmail.com>
2024-02-09 12:05:27 +0530

    Added images for the tiles

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -10,25 +10,25 @@ const AboutUsPage = () => {
                     <p className="text-3xl font-bold">Transforming Living Spaces: Redefining Comfort and Community</p>
                     <p>Discover a new era of living at our apartment community, where innovation intertwines seamlessly with comfort. We're dedicated to shaping spaces that inspire connection, offering residents a haven where modern amenities and a vibrant atmosphere converge to create unforgettable experiences.</p>
                 </div>
-                <div className="relative w-1/2 border-2 border-black">
+                <div className="relative w-1/2">
                     <div className="absolute top-48 left-0">
-                        <ImageTile image_path={"../images/main_img2-min.jpg"} />
+                        <ImageTile image_path={"../images/tile3-min.jpg"} />
                     </div>
 
                     <div className="absolute bottom-0 left-56">
-                        <ImageTile image_path={"../images/main_img2-min.jpg"} />
+                        <ImageTile image_path={"../images/tile2-min.jpg"} />
                     </div>
 
                     <div className="absolute top-20 left-56">
-                        <ImageTile image_path={"../images/main_img2-min.jpg"} />
+                        <ImageTile image_path={"../images/tile-min.jpg"} />
                     </div>
 
                     <div className="absolute top-12 left-112">
-                        <ImageTile image_path={"../images/main_img2-min.jpg"} />
+                        <ImageTile image_path={"../images/tile4-min.jpg"} />
                     </div> 
 
                     <div className="absolute top-96 left-112">
-                        <ImageTile image_path={"../images/main_img2-min.jpg"} />
+                        <ImageTile image_path={"../images/tile5.jpg"} />
                     </div> 
                 </div>
             </div>
