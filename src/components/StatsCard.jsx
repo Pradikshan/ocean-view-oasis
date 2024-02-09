1760544e2b945cedf444106e22fe64c4c8f9f153
Pradikshan <pradikshan234@gmail.com>
2024-02-09 16:26:08 +0530

    Added layout and spacing

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -1,6 +1,9 @@
 const StatsCard = ({ stat, subtext }) => {
     return (
-
+        <div className="m-5">
+            <p className="text-2xl font-bold py-3">{stat}</p>
+            <p>{subtext}</p>
+        </div>
     );
 }
 
