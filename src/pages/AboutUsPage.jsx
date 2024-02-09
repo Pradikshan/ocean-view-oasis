6feae05be09b6c504fb82e29433442ad18ac0166
Pradikshan <pradikshan234@gmail.com>
2024-02-09 16:16:12 +0530

    Added conditional check for DOM elements

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -10,26 +10,40 @@ const AboutUsPage = () => {
                     <p className="text-2xl md:text-3xl font-bold">Transforming Living Spaces: Redefining Comfort and Community</p>
                     <p>Discover a new era of living at our apartment community, where innovation intertwines seamlessly with comfort. We're dedicated to shaping spaces that inspire connection, offering residents a haven where modern amenities and a vibrant atmosphere converge to create unforgettable experiences.</p>
                 </div>
-                <div className="relative md:w-1/2 py-20 border-2 border-black">
-                    <div className="absolute top-0 md:top-48 left-0">
+                <div className="relative md:w-1/2 border-2 border-black">
+                    {window.innerWidth >= 768 ? (
+                    <>
+                    <div className="absolute md:top-48 md:left-0">
                         <ImageTile image_path={"../images/tile3-min.jpg"} />
                     </div>
 
                     <div className="absolute md:bottom-0 md:left-56">
                         <ImageTile image_path={"../images/tile2-min.jpg"} />
                     </div>
+                    </>
+                    )
+                    : null
+                    }
 
-                    <div className="absolute md:top-20 md:left-56">
+                    <div className="absolute -top-5 -left-20 md:top-20 md:left-56">
                         <ImageTile image_path={"../images/tile-min.jpg"} />
                     </div>
 
-                    <div className="absolute md:top-12 md:left-112">
+                    <div className="absolute top-0 right-0 md:top-12 md:left-112">
                         <ImageTile image_path={"../images/tile4-min.jpg"} />
                     </div> 
 
+                    {window.innerWidth >= 768 ? (
+                    <>
                     <div className="absolute md:top-96 md:left-112">
                         <ImageTile image_path={"../images/tile5.jpg"} />
                     </div> 
+                    </>
+                    )
+                    : null
+                    }
+
+                    
                 </div>
             </div>
         </div>
