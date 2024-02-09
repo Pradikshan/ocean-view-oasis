48c90da594c499b3f03ba36232b902ccf81adc9a
Pradikshan <pradikshan234@gmail.com>
2024-02-09 18:42:27 +0530

    Added layout and spacing

--- src/components/Profilecard.jsx
+++ src/components/Profilecard.jsx
@@ -1,5 +1,10 @@
 const ProfileCard = ({ image_path, name, position }) => {
     return (
+        <div className="flex flex-col place-items-center my-5">
+            <img src={image_path} alt="team member" className="w-24 h-24 rounded-full object-cover" />
+            <p className="font-bold">{name}</p>
+            <p>{position}</p>
+        </div>
 
     );
 }
