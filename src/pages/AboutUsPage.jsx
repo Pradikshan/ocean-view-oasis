16c31a67bca1910a25ecf69db6532988887190f8
Pradikshan <pradikshan234@gmail.com>
2024-02-09 21:28:06 +0530

    Made the layout responsive

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -52,41 +52,41 @@ const AboutUsPage = () => {
             </div>
             
 
-            <div className="flex flex-row px-12 mt-28">
-                    <div className="flex flex-col w-1/2">
-                        <p className="text-3xl font-bold">Our Mission</p>
-                        <p className="text-lg mt-2">At Ocean View Oasis, our mission is to provide a sanctuary by the sea, where residents can embrace the beauty of coastal living while enjoying modern comforts and fostering a strong sense of community. We are committed to creating an environment that promotes well-being, connection, and sustainable living.</p>
-                        <br/>
-                        <p>Central to our mission is the cultivation of a vibrant community where neighbors become friends and shared experiences become cherished memories. We believe that fostering meaningful connections enriches not only individual lives but also the collective well-being of our community as a whole.</p>
-                        <br/>
-                        <p>Moreover, we are dedicated to promoting sustainability and environmental stewardship in every aspect of our operations. From energy-efficient design to eco-conscious practices, we strive to minimize our ecological footprint and preserve the natural beauty that surrounds us for generations to come.</p>
-                        <br/>
-                        <p>Our commitment extends beyond providing a place to live; it encompasses creating a lifestyle where residents can thrive, find joy in everyday moments, and forge lasting connections. We invite you to join us on this journey towards coastal living at its finest.</p>
-                    </div>
+            <div className="flex flex-col md:flex-row px-5 md:px-12 mt-28">
+                <div className="flex flex-col md:w-1/2">
+                    <p className="text-2xl md:text-3xl font-bold">Our Mission</p>
+                    <p className="md:text-lg mt-2">At Ocean View Oasis, our mission is to provide a sanctuary by the sea, where residents can embrace the beauty of coastal living while enjoying modern comforts and fostering a strong sense of community. We are committed to creating an environment that promotes well-being, connection, and sustainable living.</p>
+                    <br/>
+                    <p>Central to our mission is the cultivation of a vibrant community where neighbors become friends and shared experiences become cherished memories. We believe that fostering meaningful connections enriches not only individual lives but also the collective well-being of our community as a whole.</p>
+                    <br/>
+                    <p>Moreover, we are dedicated to promoting sustainability and environmental stewardship in every aspect of our operations. From energy-efficient design to eco-conscious practices, we strive to minimize our ecological footprint and preserve the natural beauty that surrounds us for generations to come.</p>
+                    <br/>
+                    <p className="mb-8">Our commitment extends beyond providing a place to live; it encompasses creating a lifestyle where residents can thrive, find joy in everyday moments, and forge lasting connections. We invite you to join us on this journey towards coastal living at its finest.</p>
+                </div>
 
-                    <div className="flex flex-col w-1/3 ms-auto">
-                        <StatsCard 
-                        stat={"$44 Million in Assets"}
-                        subtext={"Reflecting our substantial investment"} />
+                <div className="flex flex-col md:w-1/3 md:ms-auto">
+                    <StatsCard 
+                    stat={"$44 Million in Assets"}
+                    subtext={"Reflecting our substantial investment"} />
 
-                        <StatsCard 
-                        stat={"99% Occupancy Rate"}
-                        subtext={"High demand for our premium apartments"} />   
+                    <StatsCard 
+                    stat={"99% Occupancy Rate"}
+                    subtext={"High demand for our premium apartments"} />   
 
-                        <StatsCard 
-                        stat={"10 Years of Excellence"}
-                        subtext={"A decade of dedicated service"} />
-                        
-                        <StatsCard 
-                        stat={"25,000 Square Feet of Green Space"}
-                        subtext={"Ample space for relaxation"} />
-                    </div>
+                    <StatsCard 
+                    stat={"10 Years of Excellence"}
+                    subtext={"A decade of dedicated service"} />
+                    
+                    <StatsCard 
+                    stat={"25,000 Square Feet of Green Space"}
+                    subtext={"Ample space for relaxation"} />
+                </div>
             </div>
 
-            <div className="px-12 mt-28">
-                <p className="text-3xl font-bold">Our values</p>
+            <div className="px-5 md:px-12 mt-28">
+                <p className="text-2xl md:text-3xl font-bold">Our values</p>
                 <p className="mt-2">Guided by our core values, Ocean View Oasis is more than just a place to live; it's a reflection of our commitment to excellence, integrity, and community. These values shape every aspect of our operations, from the design of our residences to the experiences we create for our residents.</p>
-                <div className="grid grid-cols-3 mt-8">   
+                <div className="grid grid-cols-1 md:grid-cols-3 mt-8">   
                     <TextCard 
                     heading={"Excellence"}
                     text={"We strive for excellence in all that we do, from the meticulous craftsmanship of our apartments to the attentive service we provide to our residents."} />
@@ -117,11 +117,11 @@ const AboutUsPage = () => {
 
             {/* <div className="spacer layer"></div> */}
 
-            <div className="px-12 mt-28 mb-32">
+            <div className="px-5 md:px-12 mt-28 mb-32">
                 <p className="text-3xl font-bold">Our team</p>
                 <p className="mt-2">Our dedicated team at Ocean View Oasis is committed to providing exceptional service and creating an unparalleled living experience for our residents, ensuring that every interaction is infused with warmth, professionalism, and genuine care.</p>
 
-                <div className="grid grid-cols-5 mt-12">
+                <div className="grid grid-cols-2 md:grid-cols-5 mt-12">
                     <ProfileCard 
                     image_path={"../images/rust.jpg"} 
                     name={"Rustin Spencer Cohle"} 
