0dc6b5ebdf6c2e5ecb808862bdec4509b65b4283
Pradikshan <pradikshan234@gmail.com>
2024-02-09 16:35:37 +0530

    Added more content to the mission statement

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,4 +1,5 @@
 import ImageTile from "../components/ImageTile";
+import StatsCard from "../components/StatsCard";
 
 
 const AboutUsPage = () => {
@@ -47,12 +48,24 @@ const AboutUsPage = () => {
                 </div>
             </div>
 
-            <div className="flex flex-row px-12">
-                <div className="flex flex-col">
-                    <p className="text-3xl font-bold">Our Mission</p>
-                    <p>At Ocean View Oasis, our mission is to provide a sanctuary by the sea, where residents can embrace the beauty of coastal living while enjoying modern comforts and fostering a strong sense of community. We are committed to creating an environment that promotes well-being, connection, and sustainable living.</p>
-                </div>
+            <div className="flex flex-row px-12 mt-28 justify-between">
+                
+                    <div className="flex flex-col w-1/2">
+                        <p className="text-3xl font-bold">Our Mission</p>
+                        <p className="text-lg mt-2">At Ocean View Oasis, our mission is to provide a sanctuary by the sea, where residents can embrace the beauty of coastal living while enjoying modern comforts and fostering a strong sense of community. We are committed to creating an environment that promotes well-being, connection, and sustainable living.</p>
+                        <br/>
+                        <p>Central to our mission is the cultivation of a vibrant community where neighbors become friends and shared experiences become cherished memories. We believe that fostering meaningful connections enriches not only individual lives but also the collective well-being of our community as a whole.</p>
+                        <br/>
+                        <p>Moreover, we are dedicated to promoting sustainability and environmental stewardship in every aspect of our operations. From energy-efficient design to eco-conscious practices, we strive to minimize our ecological footprint and preserve the natural beauty that surrounds us for generations to come.</p>
+                        <br/>
+                        <p>Our commitment extends beyond providing a place to live; it encompasses creating a lifestyle where residents can thrive, find joy in everyday moments, and forge lasting connections. We invite you to join us on this journey towards coastal living at its finest.</p>
+                    </div>
 
+                    <div className="flex flex-col w-1/3">
+                        <StatsCard 
+                        stat={"$44 Million in Assets"}
+                        subtext={"Reflecting our substantial investment in creating top-notch living spaces and amenities"} />
+                    </div>
                 
 
 
