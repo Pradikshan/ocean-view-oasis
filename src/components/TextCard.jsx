a9be63cd8eeeab9eaeaf0d118ed0408943b170ee
Pradikshan <pradikshan234@gmail.com>
2024-02-09 16:52:52 +0530

    Implemented layout and spacing

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -1,6 +1,9 @@
 const TextCard = ({ heading, text }) => {
     return (
-
+        <div>
+            <p className="text-xl font-bold">{heading}</p>
+            <p className="mt-2">{text}</p>
+        </div>
     );
 }
 
