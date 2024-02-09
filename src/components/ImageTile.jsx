9a110b2ab4c374da36b1979abc3943abbb4e955d
Pradikshan <pradikshan234@gmail.com>
2024-02-09 21:45:35 +0530

    Adjusted the spacing

--- src/components/ImageTile.jsx
+++ src/components/ImageTile.jsx
@@ -2,7 +2,7 @@ const ImageTile = ({ image_path }) => {
     return (
         <>
         <div>
-            <img src={image_path} alt="tile" className="rounded-3xl w-52 h-80 object-cover"/>
+            <img src={image_path} alt="tile" className="rounded-3xl md:w-52 md:h-80 object-cover"/>
         </div>
         </>
     );
