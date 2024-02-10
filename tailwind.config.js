456edb750e01812f1fbc79927239149d15aeea49
Pradikshan <pradikshan234@gmail.com>
2024-02-10 15:48:00 +0530

    Changed the background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,7 @@ module.exports = {
       },
       backgroundImage: {
         "main-img": "url(../public/images/main_img2-min.jpg)",
-        "about-img": "url(../public/images/background-img-about.svg)",
-        "about-img2": "url(../public/images/background-img-about2.svg)",
+        "about-img": "url(../public/images/background-img-about2.svg)",
       },
       backgroundColor: {
         "trans": "#000000e1",
