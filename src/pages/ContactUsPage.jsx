2e258434deb053cecf1de5ea9c31b6e7c157639e
Pradikshan <pradikshan234@gmail.com>
2024-02-10 16:33:45 +0530

    Added background image

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -12,31 +12,33 @@ const ContactUsPage = () => {
         margin_top={"-mt-8"}
         heading={"Contact Us"} 
         subheading={"Reach out to us – let's start a conversation about turning your dreams of coastal living into a reality"} /> */}
+        <div className="relative bg-contact-img bg-cover">
+            
+            <div className="pt-8 pb-5 md:pt-16 md:pb-20">
+                <SplitLayout>
+                    <div className="grid grid-cols-1 md:grid-cols-2">
+                        <div className="px-6 mb-10">
+                            <p className="text-2xl md:text-3xl font-bold">Reach out to us at:</p>
+                            <div className="flex flex-row items-center mt-5">
+                                <img src="../images/telephone.png" alt="telephone" className="w-8 h-8"/>
+                                <p className="mx-2 font-bold">+94 76 5944 123</p>
+                            </div>
 
-        <div className="pt-8 pb-5 md:pt-16 md:pb-20">
-            <SplitLayout>
-                <div className="grid grid-cols-1 md:grid-cols-2">
-                    <div className="px-6 mb-10">
-                        <p className="text-2xl md:text-3xl font-bold">Reach out to us at:</p>
-                        <div className="flex flex-row items-center mt-5">
-                            <img src="../images/telephone.png" alt="telephone" className="w-8 h-8"/>
-                            <p className="mx-2 font-bold">+94 76 5944 123</p>
+                            <div className="flex flex-row items-center mt-5">
+                                <img src="../images/email.png" alt="telephone" className="w-8 h-8"/>
+                                <p className="mx-2 font-bold">oceanview@oasis.com</p>
+                            </div>
                         </div>
-
-                        <div className="flex flex-row items-center mt-5">
-                            <img src="../images/email.png" alt="telephone" className="w-8 h-8"/>
-                            <p className="mx-2 font-bold">oceanview@oasis.com</p>
-                        </div>
+                        {window.innerWidth >= 768 ? (
+                            <div className="ms-80 h-full w-0 border-2 border-black items-end"></div>
+                        ) : null}
                     </div>
-                    {window.innerWidth >= 768 ? (
-                        <div className="ms-80 h-full w-0 border-2 border-black items-end"></div>
-                    ) : null}
-                </div>
 
-                <div className="px-6 md:ps-20">
-                    <Inquiry />
-                </div>
-            </SplitLayout>
+                    <div className="px-6 md:ps-20">
+                        <Inquiry />
+                    </div>
+                </SplitLayout>
+            </div>
         </div>
         </>
     );
