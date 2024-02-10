7bdb3e708aef98bb881e1aa708629dcbdaa929f4
Pradikshan <pradikshan234@gmail.com>
2024-02-10 14:42:47 +0530

    Fixed indentation

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -55,36 +55,36 @@ const AboutUsPage = () => {
             
             
 
-                <div className="flex flex-col md:flex-row px-5 md:px-12 mt-10 md:pt-28">
-                    <div className="flex flex-col md:w-1/2">
-                        <p className="text-2xl md:text-3xl font-bold">Our Mission</p>
-                        <p className="md:text-lg mt-2">At Ocean View Oasis, our mission is to provide a sanctuary by the sea, where residents can embrace the beauty of coastal living while enjoying modern comforts and fostering a strong sense of community. We are committed to creating an environment that promotes well-being, connection, and sustainable living.</p>
-                        <br/>
-                        <p>Central to our mission is the cultivation of a vibrant community where neighbors become friends and shared experiences become cherished memories. We believe that fostering meaningful connections enriches not only individual lives but also the collective well-being of our community as a whole.</p>
-                        <br/>
-                        <p>Moreover, we are dedicated to promoting sustainability and environmental stewardship in every aspect of our operations. From energy-efficient design to eco-conscious practices, we strive to minimize our ecological footprint and preserve the natural beauty that surrounds us for generations to come.</p>
-                        <br/>
-                        <p className="mb-8">Our commitment extends beyond providing a place to live; it encompasses creating a lifestyle where residents can thrive, find joy in everyday moments, and forge lasting connections. We invite you to join us on this journey towards coastal living at its finest.</p>
-                    </div>
-
-                    <div className="flex flex-col md:w-1/3 md:ms-auto">
-                        <StatsCard 
-                        stat={"$44 Million in Assets"}
-                        subtext={"Reflecting our substantial investment"} />
-
-                        <StatsCard 
-                        stat={"99% Occupancy Rate"}
-                        subtext={"High demand for our premium apartments"} />   
-
-                        <StatsCard 
-                        stat={"10 Years of Excellence"}
-                        subtext={"A decade of dedicated service"} />
-                        
-                        <StatsCard 
-                        stat={"25,000 Square Feet of Green Space"}
-                        subtext={"Ample space for relaxation"} />
-                    </div>
+            <div className="flex flex-col md:flex-row px-5 md:px-12 mt-10 md:pt-28">
+                <div className="flex flex-col md:w-1/2">
+                    <p className="text-2xl md:text-3xl font-bold">Our Mission</p>
+                    <p className="md:text-lg mt-2">At Ocean View Oasis, our mission is to provide a sanctuary by the sea, where residents can embrace the beauty of coastal living while enjoying modern comforts and fostering a strong sense of community. We are committed to creating an environment that promotes well-being, connection, and sustainable living.</p>
+                    <br/>
+                    <p>Central to our mission is the cultivation of a vibrant community where neighbors become friends and shared experiences become cherished memories. We believe that fostering meaningful connections enriches not only individual lives but also the collective well-being of our community as a whole.</p>
+                    <br/>
+                    <p>Moreover, we are dedicated to promoting sustainability and environmental stewardship in every aspect of our operations. From energy-efficient design to eco-conscious practices, we strive to minimize our ecological footprint and preserve the natural beauty that surrounds us for generations to come.</p>
+                    <br/>
+                    <p className="mb-8">Our commitment extends beyond providing a place to live; it encompasses creating a lifestyle where residents can thrive, find joy in everyday moments, and forge lasting connections. We invite you to join us on this journey towards coastal living at its finest.</p>
                 </div>
+
+                <div className="flex flex-col md:w-1/3 md:ms-auto">
+                    <StatsCard 
+                    stat={"$44 Million in Assets"}
+                    subtext={"Reflecting our substantial investment"} />
+
+                    <StatsCard 
+                    stat={"99% Occupancy Rate"}
+                    subtext={"High demand for our premium apartments"} />   
+
+                    <StatsCard 
+                    stat={"10 Years of Excellence"}
+                    subtext={"A decade of dedicated service"} />
+                    
+                    <StatsCard 
+                    stat={"25,000 Square Feet of Green Space"}
+                    subtext={"Ample space for relaxation"} />
+                </div>
+            </div>
 
                 
 
