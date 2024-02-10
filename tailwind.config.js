b4158644ab4d0db52ae3857d34850c22ab1d3edd
Pradikshan <pradikshan234@gmail.com>
2024-02-10 16:44:34 +0530

    Added path for floor plan background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
         "main-img": "url(../public/images/main_img2-min.jpg)",
         "about-img": "url(../public/images/background-img-about2.svg)",
         "faq-img": "url(../public/images/background-img-faq.svg)",
-        "contact-img": "url(../public/images/background-img-contact.svg)",  
+        "contact-img": "url(../public/images/background-img-contact.svg)", 
+        "floor-img": "url(../public/images/background-img-floor.svg)",  
       },
       backgroundColor: {
         "trans": "#000000e1",
