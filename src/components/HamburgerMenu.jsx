24b0f90daf57745612862888b8968c712af5ebc9
Pradikshan <pradikshan234@gmail.com>
2024-02-11 11:52:54 +0530

    Removed commented out code

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -31,21 +31,6 @@ const HamburgerMenu = () => {
 
     return (
         <>
-        {/* {!toggleMenu && (
-            <div className="flex flex-row">
-                <button className="ms-8 -mt-2" onClick={openMenu}>
-                    <GiHamburgerMenu style={{ fontSize: 24 }}/>
-                </button>
-
-                <div className="flex flex-row ms-8">
-                <img src="../images/logo.png" alt="logo" className="w-16 h-16"/>
-                <p className="self-center -ms-1 -mt-1 text-lg font-bold">OCEAN VIEW OASIS</p>
-                </div>
-    
-    
-            </div>
-
-        )} */}
         <div className="lg:hidden flex flex-row">
             <button className="ms-8 -mt-2" onClick={openMenu}>
                 <GiHamburgerMenu style={{ fontSize: 24 }}/>
@@ -60,46 +45,49 @@ const HamburgerMenu = () => {
 
         {toggleMenu && (
             <AnimatePresence>
-                <motion.div 
-                className="absolute z-10 w-3/4 h-full bg-secondary-color"
-                onClick={(e) => e.stopPropagation()}
-                variants={slideIn}
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                >
+                <Backdrop onClick={closeMenu}>
+                    <motion.div 
+                    className="absolute z-10 w-3/4 h-full bg-secondary-color"
+                    onClick={(e) => e.stopPropagation()}
+                    variants={slideIn}
+                    initial="hidden"
+                    animate="visible"
+                    exit="exit"
+                    >
 
-                <div className="flex flex-col text-black">
-                    <IoClose 
-                    style={{fontSize: "30px"}}
-                    className="ms-auto mx-1 mt-2" 
-                    onClick={closeMenu} />
-                    <ul className="flex flex-col mt-5 mobile">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/floor_plan">Floor plan</Link>
-                        </li>
-                        <li>
-                            <Link to="/pricing">Pricing</Link>
-                        </li>
-                        <li>
-                            <Link to="/photo_gallery">Photo gallery</Link> 
-                        </li>
-                        <li>
-                            <Link to="/faq">FAQ</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact_us">Contact us</Link>
-                        </li>
-                    </ul>
+                    <div className="flex flex-col text-black">
+                        <IoClose 
+                        style={{fontSize: "30px"}}
+                        className="ms-auto mx-1 mt-2" 
+                        onClick={closeMenu} />
+                        <ul className="flex flex-col mt-5 mobile">
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/floor_plan">Floor plan</Link>
+                            </li>
+                            <li>
+                                <Link to="/pricing">Pricing</Link>
+                            </li>
+                            <li>
+                                <Link to="/photo_gallery">Photo gallery</Link> 
+                            </li>
+                            <li>
+                                <Link to="/faq">FAQ</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact_us">Contact us</Link>
+                            </li>
+                        </ul>
 
-                </div>
-                </motion.div>
+                    </div>
+                    </motion.div>
+                </Backdrop>
+                
             </AnimatePresence>
         )}
 
