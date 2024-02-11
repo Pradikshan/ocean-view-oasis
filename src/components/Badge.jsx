54f22ddd06927f5362bd27642bf260f9803d70b1
Pradikshan <pradikshan234@gmail.com>
2024-02-11 13:47:05 +0530

    Adjusted font size and width

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,7 +1,7 @@
 const Badge = ({ text }) => {
     return (
-        <div className="w-48 rounded-full p-3 m-3 shadow-2xl bg-slate-600">
-            <p className="text-center text-white text-lg font-bold">{text}</p>
+        <div className="w-40 rounded-full p-3 m-3 shadow-2xl bg-slate-600">
+            <p className="text-center text-white text-md md:text-sm font-bold">{text}</p>
         </div>
     );
 }
