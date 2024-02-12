b4577c0562c4b0343cfd5bbdf8096d9d4c64dfa9
Pradikshan <pradikshan234@gmail.com>
2024-02-12 15:29:26 +0530

    Changed the font size

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -10,15 +10,15 @@ const TestimonialCard = ({ className, content_style, image, name, title, testimo
 
         <div className={`testimonial-card ${className}`}>
             <div className="px-8 py-3">
-                <p className={`pt-4 ${content_style}`}>{testimonial}</p>
+                <p className={`pt-4 text-sm ${content_style}`}>{testimonial}</p>
             </div>
             
             <div className="px-8 py-3 mt-auto">
                 <div className="flex flex-row mt-8">
                     <img src={image} alt="resident" className="rounded-full object-cover w-14 h-14" />
                     <div className="flex flex-col ms-5">
-                        <p className="text-lg font-bold">{name}</p>
-                        <p>{title}</p>
+                        <p className="font-bold">{name}</p>
+                        <p className="text-sm">{title}</p>
                     </div> 
                 </div>
             </div>
